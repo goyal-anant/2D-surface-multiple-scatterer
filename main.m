e3e03d46a439def5684e07de6d388795e44fabb9
goyal_anant <learn.goyal@gmail.com>
2022-09-18 13:21:36 +0530

    initial commit

---
+++ main.m
@@ -0,0 +1,12 @@
+clear;
+
+%% defining the constants
+
+%% building the scatterers
+%here we will make two cylinders in the domain
+
+%% formulating the problem
+
+%% plotting the result
+
+%% defining functions
